f0c11672aa8a9f1847bfa23f7839c81dff8ba4b2
atuy <atuy@onsw.net>
2019-08-03 21:00:11 +0900

    メインウィンドウの作成までを実装 コピペ機能はまだ未実装

---
+++ ExpClip/ClipControl.h
@@ -0,0 +1,28 @@
+#ifndef __CLIPCONTROL_H__
+#define __CLIPCONTROL_H__
+
+#include <Windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+//-------------------------------------------------------------
+// 概要   : 
+// 備考   : 
+//------------------------------------------------------------- 
+class ClipControl {
+private:
+public:
+	// コンストラクタ
+	ClipControl();
+
+
+public:
+	// 初期タブの準備
+	void InitTab();
+
+};
+
+
+#endif
--- ExpClip/ExpClip.cpp
+++ ExpClip/ExpClip.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <CommCtrl.h>
 #include "resource.h"
+#include "ClipControl.h"
 
 //==============================================================
 // 定数
@@ -19,8 +20,11 @@ HINSTANCE   hInst;		  // インスタンスハンドル
 //==============================================================
 // 関数プロトタイプ宣言
 //==============================================================
+LPARAM CALLBACK MainWndProc(HWND hWnd, UINT uMessage,
+	WPARAM wParam, LPARAM lParam);
 LPARAM CALLBACK MainDlgProc(HWND hWnd, UINT uMessage,
 							 WPARAM wParam, LPARAM lParam);
+bool RegisterNotify(HWND hWnd);
 
 
 //--------------------------------------------------------------
@@ -36,7 +40,7 @@ HWND MakeTabControl(HWND hWnd, WPARAM wp, LPARAM lp)
 	TCHAR sTab1_name[] = TEXT("aaa");
 	TCHAR sTab2_name[] = TEXT("Tokyo mew mew");
 
-	hTab = GetDlgItem(hWnd, IDC_TAB1);
+	hTab = GetDlgItem(hWnd, TABC_MAIN);
 
 	tcItem.mask = TCIF_TEXT;
 	tcItem.pszText = sTab1_name;
@@ -53,8 +57,8 @@ HWND MakeTabControl(HWND hWnd, WPARAM wp, LPARAM lp)
 // 戻り値:
 // 引数:
 //--------------------------------------------------------------
-int APIENTRY WinMain(HINSTANCE hInstCurrent, HINSTANCE hinstPrevious,
-					 LPSTR lpszCmdLine, int nCmdShow)
+int APIENTRY wWinMain(HINSTANCE hInstCurrent, HINSTANCE hinstPrevious,
+					 LPWSTR lpszCmdLine, int nCmdShow)
 {
 	MSG        msg;
 	WNDCLASS   wndclass;
@@ -63,12 +67,12 @@ int APIENTRY WinMain(HINSTANCE hInstCurrent, HINSTANCE hinstPrevious,
 
 	//メインウインドウクラスの登録
 	wndclass.style         = 0;
-	wndclass.lpfnWndProc   = MainDlgProc;
+	wndclass.lpfnWndProc   = MainWndProc;
 	wndclass.cbClsExtra    = 0;
 	wndclass.cbWndExtra    = DLGWINDOWEXTRA;
 	wndclass.hInstance     = hInst;
-	wndclass.hIcon         = LoadIcon(NULL,IDI_APPLICATION);
-	wndclass.hCursor       = LoadCursor(NULL,IDC_ARROW);
+	wndclass.hIcon         = LoadIcon(hInst, TEXT("ICON_MAIN"));
+	wndclass.hCursor       = LoadCursor(NULL, IDC_ARROW);
 	wndclass.hbrBackground = (HBRUSH)(COLOR_BTNFACE+1);
 	wndclass.lpszMenuName  = NULL;
 	wndclass.lpszClassName = szClassName;
@@ -80,31 +84,114 @@ int APIENTRY WinMain(HINSTANCE hInstCurrent, HINSTANCE hinstPrevious,
 	}
 	
 	//メインウインドウの生成
-	hDlg = CreateDialog(hInst,
-		MAKEINTRESOURCE(IDD_DIALOG1), 0, (DLGPROC)MainDlgProc);
-	if(hDlg == NULL)
+	HWND hWndMain;
+	hWndMain = CreateWindowEx(
+		0, //WS_EX_TRANSPARENT,
+		szClassName,
+		szAppName,
+		0, //WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		100, 100,
+		NULL,
+		NULL,
+		hInst,
+		NULL
+	);
+	
+	if (hWndMain == NULL)
 	{
-		MessageBox(NULL,TEXT("メインウインドウを作成できませんでした"),
-				szAppName,MB_APPLMODAL | MB_ICONSTOP);
+		MessageBox(NULL, TEXT("メインウインドウを作成できませんでした"),
+			szAppName, MB_APPLMODAL | MB_ICONSTOP);
 		return FALSE;
 	}
+
+	HWND hDlg;
+	hDlg = CreateDialog(hInst,
+		MAKEINTRESOURCE(DLG_MAIN), hWndMain, (DLGPROC)MainDlgProc);
+
+	// タスクトレイに登録
+	RegisterNotify(hWndMain);
+
+
+	// ClipControlの準備
+	ClipControl *cc;
+	cc = new ClipControl();
+
 	
 	//メインウインドウの表示
+	ShowWindow(hWndMain, SW_HIDE);
 	ShowWindow(hDlg, nCmdShow);
+	UpdateWindow(hWndMain);
 	UpdateWindow(hDlg);
 
 	//メッセージループ
 	while(GetMessage(&msg, NULL, 0, 0))
 	{
-		if(IsDialogMessage(hDlg, &msg) == FALSE)
-		{
+		//if(IsDialogMessage(hDlg, &msg) == FALSE)
+		//{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
-		}
+		//}
 	}
 
 	return msg.wParam;
 }
+
+//--------------------------------------------------------------
+// 概要: メインウィンドウプロシージャ
+// 戻り値:
+// 引数:
+//--------------------------------------------------------------
+LPARAM CALLBACK MainWndProc(HWND hWnd, UINT uMessage,
+	WPARAM wParam, LPARAM lParam)
+{
+	int rc;
+
+	switch (uMessage) {
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		return DefWindowProc(hWnd, uMessage, wParam, lParam);
+	}
+
+	return FALSE;
+}
+
+//--------------------------------------------------------------
+// 概要：タスクトレイへの登録
+// 戻り値：
+// 引数：
+//--------------------------------------------------------------
+bool RegisterNotify(HWND hWnd)
+{
+	NOTIFYICONDATA ni;
+	memset(&ni, 0, sizeof(NOTIFYICONDATA));
+
+	ni.cbSize = sizeof(NOTIFYICONDATA);
+	ni.hWnd = hWnd;
+
+	ni.uID = 1001;
+
+	ni.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+
+	// icon ファイルを読み込む
+	ni.hIcon = (HICON)LoadImage(0, "icon.ico", IMAGE_ICON, 0, 0, LR_LOADFROMFILE);
+
+	// タスクトレイのアイコンにマウスイベントが発生したときにくるアプリケーション定義のメッセージ
+	// WARNING
+	//   WM_APP 以上の数値でないと message が通知されない
+	ni.uCallbackMessage = WM_APP + 1;
+
+	// タスクトレイにアイコンを追加したら自分のウィンドウを隠す。
+	Shell_NotifyIcon(NIM_ADD, &ni);
+
+	return 0;
+}
+
+
 
 //--------------------------------------------------------------
 // 概要: メインダイアログプロシージャ
@@ -114,9 +201,13 @@ int APIENTRY WinMain(HINSTANCE hInstCurrent, HINSTANCE hinstPrevious,
 LPARAM CALLBACK MainDlgProc(HWND hWnd, UINT uMessage,
 							 WPARAM wParam, LPARAM lParam)
 {
+	int rc = 0;
+
 	switch(uMessage){
 	//ウインドウ作成時の処理
+	case WM_CREATE:
 	case WM_INITDIALOG:
+
 		MakeTabControl(hWnd, wParam, lParam);
 		SetWindowText(hWnd, szAppName);
 		break;
@@ -126,17 +217,16 @@ LPARAM CALLBACK MainDlgProc(HWND hWnd, UINT uMessage,
 		switch(LOWORD(wParam)){
 		//終了
 		case IDCANCEL:
-			DestroyWindow(hWnd);
 			break;
-		
-		default:
-			return DefWindowProc(hWnd, uMessage, wParam, lParam);
 		}
 		break;
 		
 	//[X]ボタン
 	case WM_CLOSE:
-		PostMessage(hWnd, WM_COMMAND, IDCANCEL, 0);
+		rc = MessageBox(NULL, TEXT("終了しますか？"), TEXT("確認"), MB_YESNO);
+		if (rc == IDYES) {
+			DestroyWindow(hWnd);
+		}
 		break;
 	
 	//プログラム終了時のウインドウ破棄
@@ -144,8 +234,6 @@ LPARAM CALLBACK MainDlgProc(HWND hWnd, UINT uMessage,
 		PostQuitMessage(0);
 		break;
 
-	default:
-		return DefWindowProc(hWnd, uMessage, wParam, lParam);
 	}
 	
 	return FALSE;
